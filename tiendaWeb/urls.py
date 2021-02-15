6bd0c7f58a6f89af14287a3457530135067c019e
pablo <pgb287@gmail.com>
2021-02-15 01:12:36 -0300

    primer commit del proyecto

---
+++ tiendaWeb/urls.py
@@ -0,0 +1,30 @@
+"""tiendaWeb URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+#configuracion para poder ver una foto del panel del admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('home.urls')), #conexion con urls de home app   
+    path('servicios/', include('servicios.urls')), #conexion con urls de servicios app   
+    path('tienda/', include('tienda.urls')), #conexion con urls de tienda app   
+    path('blog/', include('blog.urls')), #conexion con urls de blog app   
+    path('contacto/', include('contacto.urls')), #conexion con urls de contacto app   
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #path para poder ver una foto del panel del admin
